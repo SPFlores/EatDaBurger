e89610926fe7e808fee757109bf7a7487b5eb4b5
SPFlores <sflores92886@gmail.com>
2019-05-18 14:06:25 -0700

    handlebars init, server init

---
+++ config/index.js
@@ -0,0 +1,9 @@
+const { createConnection } = require('mysql2')
+
+module.exports = createConnection({
+  host: '',
+  port: null,
+  user: '',
+  password: '',
+  database: ''
+})
--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('file working')
+const express = require('express')
+const handlebars = require('express-handlebars]')
+const { join } = require('path')
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+require('./routes')(app)
+
+require('./config').connect(_ => { app.listen(3000) })
